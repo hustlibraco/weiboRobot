66adc19dec0b5663f2adca3b4a8271c8ceb3ba14
hustlibraco <hustlibraco@gmail.com>
2016-07-31 02:14:54 +0800

    修改配置文件类型

--- config.py
+++
@@ -1,6 +0,0 @@
-API_KEY = '4256975911'
-API_SECRET = 'c18c05b7168e075fa991ed998c780005'
-REDIRECT_URI = 'http://weibo.sha256.cc'
-USERNAME = 'nexusn@126.com'
-PASSWORD = 'qwer1234!@#$' 
-WHOCANAT = 'libraco'
